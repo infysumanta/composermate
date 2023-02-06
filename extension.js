a38d900645b5693e0a00459010e46fa49e1ce199
Sumanta Kabiraj <sumantablog@gmail.com>
2023-02-06 20:48:33 +0530

    created the workflow

--- extension.js
+++ extension.js
@@ -23,12 +23,16 @@ function activate(context) {
         const items = data.results.map((result) => {
           return {
             label: result.name,
-            description: result.description,
+            name: result.name,
+            description: `Download: ${result.downloads} | Fav: ${result.favers}`,
+            detail: result.description,
+            url: result.url,
+            repository: result.repository,
           };
         });
         const quickPick = vscode.window.createQuickPick();
         quickPick.items = items;
-        quickPick.show();
+
         quickPick.onDidChangeValue(async (value) => {
           quickPick.busy = true;
           try {
@@ -48,13 +52,62 @@ function activate(context) {
             quickPick.busy = false;
           }
         });
-        quickPick.onDidAccept(() => {
-          quickPick.hide();
+
+        quickPick.onDidChangeSelection((selection) => {
+          if (selection[0]) {
+            const { name, url, repository } = selection[0];
+            quickPick.hide();
+            const repoQuickPick = vscode.window.createQuickPick();
+            const data = [
+              {
+                label: "Install Package",
+                value: "install",
+                flag: 0,
+              },
+              {
+                label: "Open Repository",
+                value: "repo",
+                flag: 1,
+              },
+              {
+                label: "Open Project Url",
+                value: "url",
+                flag: 2,
+              },
+            ];
+
+            repoQuickPick.items = data;
+            repoQuickPick.onDidChangeSelection((selection) => {
+              if (selection[0]) {
+                const { value, flag } = selection[0];
+                repoQuickPick.hide();
+                switch (flag) {
+                  case 0:
+                    //  execute the console command to install the package
+                    vscode.window.showInformationMessage(
+                      `Installing ${name} package`
+                    );
+
+                    break;
+                  case 1:
+                    vscode.env.openExternal(vscode.Uri.parse(repository));
+                    break;
+                  case 2:
+                    vscode.env.openExternal(vscode.Uri.parse(url));
+                    break;
+                  default:
+                    break;
+                }
+              }
+            });
+            repoQuickPick.show();
+          }
         });
 
         quickPick.onDidHide(() => {
           quickPick.dispose();
         });
+        quickPick.show();
       } catch (error) {
         console.error(error);
       }
